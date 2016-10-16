623e042baf3820fda6325fcf521999d12a881253
rhuanca <rhuanca@gmail.com>
2016-10-16 15:40:28 -0400

    improvement in example 9

--- src/main/java/Example9.java
+++ src/main/java/Example9.java
@@ -15,18 +15,29 @@ import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.receiver.Receiver;
+import org.apache.spark.streaming.scheduler.StreamingListener;
+import org.apache.spark.streaming.scheduler.StreamingListenerBatchCompleted;
+import org.apache.spark.streaming.scheduler.StreamingListenerBatchStarted;
+import org.apache.spark.streaming.scheduler.StreamingListenerBatchSubmitted;
+import org.apache.spark.streaming.scheduler.StreamingListenerOutputOperationCompleted;
+import org.apache.spark.streaming.scheduler.StreamingListenerOutputOperationStarted;
+import org.apache.spark.streaming.scheduler.StreamingListenerReceiverError;
+import org.apache.spark.streaming.scheduler.StreamingListenerReceiverStarted;
+import org.apache.spark.streaming.scheduler.StreamingListenerReceiverStopped;
 
 public class Example9 {
-    private static Logger logger = Logger.getLogger(Example9.class);
-    private static final String EN_WIKI_PATH =
-            "/home/rhuanca/test/enwiki-20150602-pages-articles-multistream.xml";
+	private static Logger logger = Logger.getLogger(Example9.class);
+	private static final String EN_WIKI_PATH = "/home/rhuanca/test/enwiki-20150602-pages-articles-multistream.xml";
 
-    private static final class Print implements VoidFunction<JavaRDD<Element>>, Serializable {
-        private static final long serialVersionUID = 1L;
+	private static final class Print
+			implements
+				VoidFunction<JavaRDD<Element>>,
+				Serializable {
+		private static final long serialVersionUID = 1L;
 
-        @Override
-        public void call(JavaRDD<Element> elements) throws Exception {
-        	elements.foreach(new VoidFunction<Example9.Element>() {
+		@Override
+		public void call(JavaRDD<Element> elements) throws Exception {
+			elements.foreach(new VoidFunction<Example9.Element>() {
 
 				private static final long serialVersionUID = 1L;
 
@@ -36,101 +47,123 @@ public class Example9 {
 					logger.info("text: " + t.text);
 				}
 			});
-        }
-    }
+		}
+	}
 
-    private static class Element implements Serializable {
-        private static final long serialVersionUID = 1L;
+	private static class Element implements Serializable {
+		private static final long serialVersionUID = 1L;
 
-        final public String title;
-        final public String text;
+		final public String title;
+		final public String text;
 
-        public Element(String title, String text) {
-            this.title = title;
-            this.text = text;
-        }
+		public Element(String title, String text) {
+			this.title = title;
+			this.text = text;
+		}
 
-        @Override
-        public String toString() {
-            return "Element [title=" + title + ", text=" + text + "]";
-        }
-    }
+		@Override
+		public String toString() {
+			return "Element [title=" + title + ", text=" + text + "]";
+		}
+	}
 
-    private final static class CustomReceiver extends Receiver<Element> {
-        private static final long serialVersionUID = 1L;
+	private final static class CustomReceiver extends Receiver<Element> {
+		private static final long serialVersionUID = 1L;
 
-        private String path;
+		private int duration;
+		private String path;
 
-        private CustomReceiver(String path) {
-            super(StorageLevel.MEMORY_AND_DISK_2());
-            this.path = path;
-        }
+		public CustomReceiver(String path, int duration) {
+			super(StorageLevel.MEMORY_AND_DISK_2());
+			this.duration = duration;
+			this.path = path;
+		}
 
-        public void receive() {
-            try {
-                XMLStreamReader reader = XMLInputFactory.newInstance()
-                        .createXMLStreamReader(new FileInputStream(path));
-                logger.info("Starting to read pages.");
-                while (reader.hasNext()) {
-                    if (reader.next() == XMLStreamReader.START_ELEMENT) {
-                        String elementName = reader.getLocalName();
-                        if ("page".equals(elementName)) {
-                            String title = "";
-                            String text = "";
-                            boolean readPage = true;
-                            while (reader.hasNext() && readPage) {
-                                int eventType = reader.next();
-                                switch (eventType) {
-                                    case XMLStreamReader.START_ELEMENT:
-                                        elementName = reader.getLocalName();
-                                        if (elementName.equals("title"))
-                                            title = reader.getElementText();
-                                        if (elementName.equals("text"))
-                                            text = reader.getElementText();
-                                    case XMLStreamReader.END_ELEMENT:
-                                        // logger.info(">>>> title = " + title);
-                                        // logger.info(">>>> text = " + text);
-                                        store(new Element(title, text));
-                                }
-                            }
-                        }
-                    }
-                }
-                logger.info("finished to read pages.");
+		public CustomReceiver(String path) {
+			this(path, 0);
+		}
 
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        }
+		public void receive() {
+			try {
+				XMLStreamReader reader = XMLInputFactory.newInstance()
+						.createXMLStreamReader(new FileInputStream(path));
+				logger.info("Starting to read pages.");
+				boolean run = true;
+				long startTime = duration == 0 ? 0 : System.currentTimeMillis();
+				String title = "";
+				String text = "";
+				String elementName = "";
+				boolean readPage = false;
+				while (run && reader.hasNext()) {
+					int next = reader.next();
+					logger.info(">>> run: " + run);
+					// logger.info(">>> YYYYY duration = " + duration);
+					// if (duration != 0 ) {
+					// logger.info(">>> t = " + (System.currentTimeMillis()
+					// - startTime)/1000);
+					// }
+					long t = (System.currentTimeMillis() - startTime) / 1000;
+					logger.info(">>> t: " + t);
+					if (duration != 0 && t > duration) {
+						logger.info(">>> 1-run: " + run);
+						run = false;
+						logger.info(">>> 2-run: " + run);
+					}
+					// if (next == XMLStreamReader.START_ELEMENT &&
+					// "page".equals(reader.getLocalName())) {
+					// readPage = true;
+					// } else if (readPage && next ==
+					// XMLStreamReader.START_ELEMENT) {
+					// elementName = reader.getLocalName();
+					// if (elementName.equals("title")) {
+					// title = reader.getElementText();
+					// }
+					// if (elementName.equals("text")) {
+					// text = reader.getElementText();
+					// store(new Element(title, text));
+					// readPage = false;
+					// }
+					// }
+				}
+				logger.info("finished to read pages.");
+				// onStop();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
 
-        @Override
-        public void onStop() {
-            // ignore silently
-        }
+		@Override
+		public void onStop() {
+			// ignore silently
+		}
 
-        @Override
-        public void onStart() {
-            logger.info("Starting receiver...");
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    receive();
-                }
-            }).start();
-        }
-    }
+		@Override
+		public void onStart() {
+			logger.info("Starting receiver...");
 
-    public static void main(String args[]) throws IOException, InterruptedException {
-        SparkConf conf = new SparkConf().setAppName("WikiWordCount").setMaster("local[*]");
-        JavaStreamingContext jsc = new JavaStreamingContext(conf, Durations.seconds(1));
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					receive();
+				}
+			}).start();
+		}
+	}
 
-        CustomReceiver receiver = new CustomReceiver(EN_WIKI_PATH);
+	public static void main(String args[])
+			throws IOException, InterruptedException {
+		SparkConf conf = new SparkConf().setAppName("WikiWordCount")
+				.setMaster("local[*]");
+		JavaStreamingContext jsc = new JavaStreamingContext(conf,
+				Durations.seconds(1));
+		CustomReceiver receiver = new CustomReceiver(EN_WIKI_PATH, 5);
+		JavaReceiverInputDStream<Element> s = jsc.receiverStream(receiver);
 
-        JavaReceiverInputDStream<Element> s = jsc.receiverStream(receiver);
-        s.foreachRDD(new Print());
-        jsc.start();
-        jsc.awaitTermination();
-        jsc.close();
-    }
+		s.foreachRDD(new Print());
+		jsc.start();
+		jsc.awaitTermination();
+		jsc.close();
+
+	}
 }
